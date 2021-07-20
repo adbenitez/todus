7c801e4ceb295c099f731ac885607b342cc4a80e
oleksis <oleksis.fraga@gmail.com>
2021-07-20 08:07:34 -0400

    Set encoding in response client login to ISO-8859-1 (Latin-1)

--- todus/client.py
+++ todus/client.py
@@ -135,7 +135,10 @@ class ToDusClient:
             url = "https://auth.todus.cu/v2/auth/token"
             with self.session.post(url, data=data, headers=headers) as resp:
                 resp.raise_for_status()
-                token = "".join([c for c in resp.text if c in string.printable])
+                # Default Encoding for HTML4 ISO-8859-1 (Latin-1)
+                token = "".join(
+                    c for c in resp.content.decode("latin-1") if c in string.printable
+                )
                 return token
 
         return self._run_task(task, self.timeout)
