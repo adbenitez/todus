42cca02ba0fe6125200129ef698e75d1efd8fcd2
adbenitez <asieldbenitez@gmail.com>
2021-09-16 15:51:37 -0400

    remove now unused code

--- todus/errors.py
+++ todus/errors.py
@@ -2,9 +2,5 @@ class AuthenticationError(Exception):
     """Failed to authenticate with the server."""
 
 
-class AbortError(Exception):
-    """User canceled the operation."""
-
-
 class EndOfStreamError(Exception):
     """Unexpected empty response from the xmpp server."""
--- todus/util.py
+++ todus/util.py
@@ -1,46 +1,6 @@
-import queue
 import random
 import re
 import string
-from multiprocessing import Event, Process, Queue
-
-from .errors import AbortError
-
-
-class ResultProcess(Process):
-    """A process + internal queue to get target result in other process."""
-
-    def __init__(self, target, **kwargs) -> None:
-        self._real_target = target
-        self._result_queue: Queue = Queue()
-        self._failed = Event()
-        kwargs.setdefault("daemon", True)
-        super().__init__(target=self._wrapper, **kwargs)
-
-    def _wrapper(self, *args, **kwargs) -> None:
-        try:
-            self._result_queue.put(self._real_target(*args, **kwargs))
-        except BaseException as ex:
-            self._failed.set()
-            self._result_queue.put(ex)
-
-    def abort(self) -> None:
-        """Cancel process execution."""
-        self.kill()
-        self._failed.set()
-        self._result_queue.put(AbortError())
-
-    def get_result(self, timeout: float = None, kill: bool = True):
-        """Return target result."""
-        try:
-            result = self._result_queue.get(timeout=timeout)
-        except queue.Empty as ex:
-            if kill:
-                self.kill()
-            raise TimeoutError("Operation timed out.") from ex
-        if self._failed.is_set():
-            raise result
-        return result
 
 
 def generate_token(length: int) -> str:
